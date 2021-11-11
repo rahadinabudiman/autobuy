fda8ece7050e3216ddafe07afe4999e0f51e85c7
DESKTOP-EM7KJIP\R4HA <erbees.50000@gmail.com>
2021-11-12 01:57:09 +0700

    fix bug checkout

--- main.py
+++ main.py
@@ -66,7 +66,10 @@ def InputData():
 
 # check barang jika ada
 def click(browser, xpath):
-    WebDriverWait(browser, 10).until(
+    WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.XPATH, xpath))
+    )
+    WebDriverWait(browser, 60).until(
         EC.element_to_be_clickable((By.XPATH, xpath))
     )
     browser.find_element_by_xpath(xpath).click()
@@ -74,11 +77,12 @@ def click(browser, xpath):
 
 InputData()
 browser = SetupSelenium()
-wait = WebDriverWait(browser, 60)
+wait = WebDriverWait(browser, 10)
 
 # direct login
 browser.maximize_window()
-browser.get("https://www.shopee.co.id/buyer/login")
+browser.execute_script("location.reload(true);")
+browser.get("https://www.shopee.co.id")
 
 # check login
 wait.until(EC.visibility_of_element_located(
@@ -89,15 +93,25 @@ browser.get(link_produk)
 
 # jika ada variant
 # catatan button[1] buat ganti variant
-click(browser,
-      '//*[@id="main"]/div/div[2]/div[2]/div/div[1]/div[3]/div/div[4]/div/div[3]/div/div[1]/div/button[1]')
+click(
+    browser, '//*[@id="main"]/div/div[2]/div[2]/div/div[1]/div[3]/div/div[4]/div/div[3]/div/div[1]/div/button[1]')
 
 # Beli Sekarang
-click(browser,
-      '//*[@id="main"]/div/div[2]/div[2]/div/div[1]/div[3]/div/div[5]/div/div/button[2]')
+click(
+    browser, '//*[@id="main"]/div/div[2]/div[2]/div/div[1]/div[3]/div/div[5]/div/div/button[2]')
+
+# loading ketersediaan keranjang
+# wait.until(EC.invisibility_of_element_located(
+#    (By.XPATH, '//*[@id="main"]/div/div[3]')))
+
 wait.until(EC.invisibility_of_element_located(
-    (By.XPATH, '//*[@id="main"]/div/div[3]')))
+    (By.CSS_SELECTOR, '#main > div > div.action-toast')))
+
 
 # direct ke checkout
-click(browser,
-      '/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/div[7]/button[4]')
+# click(
+#    browser, '//*[@id="main"]/div/div[2]/div[2]/div/div[3]/div[2]/div[7]/button[4]')
+
+checkout = WebDriverWait(browser, 60).until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, '#main > div > div:nth-child(2) > div._164M6a > div > div:nth-child(3) > div._2jol0L._3GVi82 > div.W2HjBQ.zzOmij > button.shopee-button-solid.shopee-button-solid--primary')))
+browser.execute_script("arguments[0].click();", checkout)
